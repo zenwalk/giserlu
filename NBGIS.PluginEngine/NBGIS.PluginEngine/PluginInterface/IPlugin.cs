3f5210026ca406f4eef06b5fbc697df6f1dfd66e
ggopen <ggopen@106c984c-474f-0410-b960-6729952a5303>
2009-03-18 06:57:16 +0000

    加入PluginInterface文件夹，重新组织工程结构

---
+++ NBGIS.PluginEngine/NBGIS.PluginEngine/PluginInterface/IPlugin.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBGIS.PluginEngine
+{
+    /// <summary>
+    /// 接口IPlugin仅起一个继承声明的作用，以说明所有的插件类型都是一种IPugin类型，只是作为一个纯粹的标志。
+    /// </summary>
+   public interface IPlugin
+    {
+
+    }
+}
