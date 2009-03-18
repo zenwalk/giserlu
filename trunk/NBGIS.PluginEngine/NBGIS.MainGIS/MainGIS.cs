5c796e28dbad0d4f333003f12dd5bc6cdd8bcacf
ggopen <ggopen@106c984c-474f-0410-b960-6729952a5303>
2009-03-18 07:43:49 +0000

    插件式GIS应用框架 主程序UI（未加功能）

---
+++ trunk/NBGIS.PluginEngine/NBGIS.MainGIS/MainGIS.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NBGIS.MainGIS
+{
+    public partial class MainGIS : Form
+    {
+        public MainGIS()
+        {
+            InitializeComponent();
+        }
+    }
+}
